b03970b88c4969a1457c4b2723d976d34cbf6cc6
kelechi <oparaiskelechi@gmail.com>
2025-03-19 13:33:19 +0100

    use mongo and fetch properties

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,19 +1,24 @@
-import properties from "@/properties.json"
-import PropertyCard from "./PropertyCard"
-import Link from "next/link"
+import PropertyCard from "./PropertyCard";
+import Link from "next/link";
+import connectDB from "@/config/database";
+import Property from "@/models/property.model";
 
-const HomeProperties = () => {
-    
-    const recentProperties = properties.slice(0, 3)
-    
+const HomeProperties = async () => {
+  await connectDB();
+
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
+
   return (
     <>
-    <section className='px-4 py-6'>
-        <div className='container-xl lg:container m-auto'>
-          <h2 className='text-3xl font-bold text-blue-500 mb-6 text-center'>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
             Recent Properties
           </h2>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {recentProperties.length === 0 ? (
               <p>No Properties Found</p>
             ) : (
@@ -25,16 +30,16 @@ const HomeProperties = () => {
         </div>
       </section>
 
-      <section className='m-auto max-w-lg my-10 px-6'>
+      <section className="m-auto max-w-lg my-10 px-6">
         <Link
-          href='/properties'
-          className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
         >
           View All Properties
         </Link>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default HomeProperties
+export default HomeProperties;
