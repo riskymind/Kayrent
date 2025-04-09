085f73b383f53144a9ac9076a3444e85b2fd08d6
kelechi <oparaiskelechi@gmail.com>
2025-04-09 10:57:38 +0100

    fix bugs

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -3,7 +3,11 @@ import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/database";
 import Property from "@/models/property.model";
 
-const PropertiesPage = async ({ searchParams: { pageSize = 9, page = 1 } }) => {
+const PropertiesPage = async ({ searchParams}) => {
+  
+  const {searchParam} = await searchParams
+  const pageSize = searchParam?.pageSize || 9
+  const page = searchParam?.page || 1
   await connectDB();
   const skip = (page - 1) * pageSize;
   const total = await Property.countDocuments({});
